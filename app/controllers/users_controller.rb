a4f352ba02fb1604f76043920cf3c17cfb47c541
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-04-28 17:02:52 +0530

    created html form for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,21 @@ class UsersController < ApplicationController
     def new 
     end
 
-    def create 
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to new_user_path
+        else
+            render :new
+        end
+        
+
     end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+    
 end
