c89f6c92ea4a19b4f264e3ed1c34ca63cb09bd64
javitocor <javitocor@gmail.com>
2020-04-29 14:55:15 +0200

    Polish final details of signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create 
-    user = User.find_by(email: params[:session][:email])
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to user
